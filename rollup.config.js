f2dec5486f650b5e2c3d7831021246493ea1cf33
Edge, Matthew <mattedgeis@gmail.com>
2016-09-11 19:55:15 -0400

    PostCSS config added. .babelrc replaced with programmatic Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,44 @@
 // Rollup plugins
 import babel from 'rollup-plugin-babel'
 import eslint from 'rollup-plugin-eslint'
+import postcss from 'rollup-plugin-postcss'
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-js';
+
+// PostCSS plugins
+import simplevars from 'postcss-simple-vars'
+import cssnext from 'postcss-cssnext'
+import cssnano from 'cssnano'
 
 export default {
-  entry: 'js/main.js',
-  dest: 'js/main.min.js',
+  entry: 'js/main.jsx',
+  dest: 'build/main.min.js',
   format: 'iife', // Immediately Invoked Function Expression
   sourceMap: 'inline',
 
   plugins: [
+    postcss({
+      plugins: [
+        simplevars(),
+        cssnext({ warnForDuplicates: false }),
+        cssnano()
+      ],
+
+      extensions: ['.css']
+    }),
+
     eslint({
       exclude: [
         '**/*.scss', '**/*.css'
       ]
     }),
+
     babel({
-      exclude: 'node_modules/**'
-    })
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: ['es2015-rollup', 'react']
+    })//,
+
+    //uglify({}, minify)
   ]
 }
